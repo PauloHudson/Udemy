321768e6f1ce21b11221325f7607ca00452c5e70
Paulo Hudson <105687302+PauloHudson@users.noreply.github.com>
2023-01-16 15:57:58 -0300

    up

--- Gulp/javascript/gulpfile.js
+++ Gulp/javascript/gulpfile.js
@@ -1,11 +1,25 @@
 const {series} = require('gulp')
 const gulp = require('gulp')
 const concat = require('gulp-concat')
-const uglify = require('gulp-ugligy')
+const uglify = require('gulp-uglify')
 const babel = require('gulp-babel')
 
 function padrao(cb){
     //selecionarar todos os arquivos com extenção .js
     gulp.src('src/**/*.js')
-    return cb
+        .pipe(babel({
+            comments: false,
+            presets: ["env"]
+        }))
+        //vai "deixar feio o código, colocar em uma linha só sem espaços e etc.."
+        .pipe(uglify())
+        //caso aconteça algum error.
+        .on('error', err => console.log(err))
+        .pipe(concat('codigo.min.js'))
+        .pipe(gulp.dest('build'))
+
+
+    return cb()
 }
+
+exports.default = series(padrao)
