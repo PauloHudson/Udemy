cee322e1763c58049723b07f8a53c13e05745c58
Paulo Hudson <105687302+PauloHudson@users.noreply.github.com>
2023-01-23 15:49:15 -0300

    up

---
+++ React/exercicios/src/componentes/primeiro.jsx
@@ -0,0 +1,8 @@
+//jxs é REACT
+//simplesmente exportando uma função
+
+import React from "react"
+
+export default function() {
+    return <h1> EXJ </h1>
+}
--- React/exercicios/src/index.js
+++ React/exercicios/src/index.js
@@ -1,12 +1,16 @@
-//importar react é o "núcleo" do framework
-import React from 'react'
+//importar react é o "núcleo" da lib
 //react dom é a parte do react que conversa com a DOM, diretamente com a página.
+import React from 'react'
 import ReactDOM from 'react-dom'
 
-
+import Primeiro from './componentes/primeiro'
 //posso passar esse elemento com parametro para uma função;;
-const elemento = <h1> React 2</h1>
-//isso na verdade é um jsx
-ReactDOM.render(elemento, document.getElementById('root'))
 
-//$('<h1>').html('react 2') algo semelhante ao jquery. 
+
+//const elemento = <h1> React 2</h1>
+
+
+//isso na verdade é um jsx
+ReactDOM.render(<Primeiro />, document.getElementById('root'))
+
+//$('<h1>').html('react 2') algo semelhante ao jquery. s
