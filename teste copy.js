d1a84c4661c5abafac02e76f0109aa09b5d680db
Paulo Hudson <105687302+PauloHudson@users.noreply.github.com>
2023-01-22 21:49:11 -0300

    up

---
+++ teste copy.js
@@ -0,0 +1,12 @@
+
+
+function tabuada(x){
+    for(let i = 1; i < 11; i++){
+    console.log(`${x} x ${i} = ${x*i} `)
+    }
+    for(let i = 1; i < 11; i++){
+        console.log(`${x} x ${i} = ${x*i} `)
+        }
+}
+
+tabuada(2)
