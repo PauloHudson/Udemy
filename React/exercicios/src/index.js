b2cc131b46193619ebda3aadf2274ea984d3fbee
Paulo Hudson <105687302+PauloHudson@users.noreply.github.com>
2023-01-23 15:32:35 -0300

    up

--- React/exercicios/src/index.js
+++ React/exercicios/src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//importar react é o "núcleo" do framework
+import React from 'react'
+//react dom é a parte do react que conversa com a DOM, diretamente com a página.
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//posso passar esse elemento com parametro para uma função;;
+const elemento = <h1> React 2</h1>
+//isso na verdade é um jsx
+ReactDOM.render(elemento, document.getElementById('root'))
+
+//$('<h1>').html('react 2') algo semelhante ao jquery. 
--- teste.js
+++
@@ -1,9 +0,0 @@
-
-
-function tabuada(x){
-    for(let i = 1; i < 11; i++){
-    console.log(`${x} x ${i} = ${x*i} `)
-    }
-}
-
-tabuada(2)
