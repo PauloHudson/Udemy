21fdd4077b6df9f310ccb007ca2ee4997bf7ebeb
Paulo Hudson <105687302+PauloHudson@users.noreply.github.com>
2023-02-02 15:03:05 -0300

    upload

--- banco-relacional/TabelaPrefeitos.sql
+++ banco-relacional/TabelaPrefeitos.sql
@@ -1,10 +1,8 @@
-CREATE TABELA IF NOT EXISTS prefeitos{
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS prefeitos(
+    id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(255) NOT NULL,
-    cidade_id int UNSIGNED,
-    PRIMARY KEY (id),
-    UNIQUE KEY (cidade_id),
-    FOREIGN KEY (cidade_id) REFERNCES cidade (id)
-}
+    cidade_id int UNSIGNED  UNIQUE KEY,
+    FOREIGN KEY (cidade_id) REFERENCES cidades (id)
+)
 
 -- cidade id não aceita repetição
